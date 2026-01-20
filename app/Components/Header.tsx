9d612199c69d0309b85da494ab809395dbe2c287
Nickspero <nspappas99@gmail.com>
2026-01-19 21:05:14 -0500

    initial

---
+++ app/Components/Header.tsx
@@ -0,0 +1,15 @@
+import "../Styles/Header.css"
+
+const Header = () => {
+  return (
+    <header>
+      <div className="title__text">
+        <a className="title" href="/">SKINSTRIC</a>
+        <p className="intro">[ INTRO ]</p>
+      </div>
+        <button>ENTER CODE</button>
+    </header>
+  )
+}
+
+export default Header
--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,24 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Header from "./Components/Header"
+import "./Styles/home.css"
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    <div className="home">
+      <Header/>
+      <div className="home__content">
+      <div className="left">
+        <img className="arrow" src="/left-arrow.svg" alt="" />
+        <p>DISCOVER A.I.</p>
+      </div>
+        <h1 className="home__title">Sophisticated skincare</h1>
+        <div className="right">
+          <p>TAKE TEST</p>
+          <img className="arrow" src="/right-arrow.svg" alt="" />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <p className="home__desc">SKINSTRIC DEVELOPED AN A.I. THAT CREATES HIGHLY PERSONALIZED ROUTINE TAILORED TO WHAT YOUR SKIN NEEDS.</p>
+        <div className="dotted dotted__left"></div>
+        <div className="dotted dotted__right"></div>
+      </div>
     </div>
   );
 }
